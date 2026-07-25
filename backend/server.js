886fdc05a89aa968b9835fae6d9fd24951cdf150
Lavi Chauhan <lavichauhan284@gmail.com>
2026-07-25 23:17:07 +0530

    work on backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import 'dotenv/config';
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
+import incomeRouter from "./routes/incomeRoute.js";
+import expenseRouter from "./routes/expenseRoute.js";
+import dashboardRouter from "./routes/dashboardRoute.js";
 
 const app = express();
 const port = 4000;
@@ -17,7 +20,13 @@ connectDB();
 
 //Routes
 //userRoute.js
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+//income router
+app.use("/api/income" , incomeRouter);
+//expense router
+app.use("/api/expense", expenseRouter);
+//dashboard router
+app.use("/api/dashboard", dashboardRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
